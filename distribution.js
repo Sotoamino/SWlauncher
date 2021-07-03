96583fd372a80f9f1723d09ff79b07675166fc37
Sotoamino <59519426+Sotoamino@users.noreply.github.com>
2021-07-03 05:29:06 +0200

    Create distribution.js

---
+++ distribution.js
@@ -0,0 +1,32 @@
+{
+    "version": "1.0.0",
+    "discord": {
+        "clientId": "385581240906022916",
+        "smallImageText": "WesterosCraft",
+        "smallImageKey": "seal-circle"
+    },
+    "java": {
+        "oracle": "http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html"
+    },
+    "rss": "https://forum.westeroscraft.com/forum/news.2/index.rss",
+    "servers": [
+        {
+            "id": "SurvivalWorld-1.16.5",
+            "name": "SurvivalWorld",
+            "description": "Survie - SkyBlock - Creatif 1.16.5",
+            "icon": "http://www.survivalworld.fr/img/uploads/theme_logo.png",
+            "version": "3.9.4",
+            "address": "play.survivalworld.fr",
+            "minecraftVersion": "1.16.5",
+            "discord": {
+                "shortId": "Production",
+                "largeImageText": "WesterosCraft Production Server",
+                "largeImageKey": "server-prod"
+            },
+            "mainServer": true,
+            "autoconnect": true,
+            "modules": [
+            ]
+        }
+    ]
+}
